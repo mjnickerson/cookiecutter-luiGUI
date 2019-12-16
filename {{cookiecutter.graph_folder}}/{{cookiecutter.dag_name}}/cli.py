d4a4090cef02dfc54243c513cba12f199c3fb5b8
mjnickerson <min021@g.harvard.edu>
2019-12-16 01:51:01 -0500

    fix(cookiecutter tempating for tasks.py

--- {{cookiecutter.graph_folder}}/{{cookiecutter.dag_name}}/cli.py
+++ {{cookiecutter.graph_folder}}/{{cookiecutter.dag_name}}/cli.py
@@ -2,14 +2,13 @@ import argparse
 from luigi import build
 from tasks.tasks import Run_{{cookiecutter.graph_folder}}
 
-
-#WE ARE AUTOFILLING THE COOKIECUTTER, BUT WE MAY NEED TO SEND ARGUMENTS FOR THE FILE NAME TO BE PROCESSED HERE!
+#WE ARE AUTOFILLING THE COOKIECUTTER, BUT IF NEEDED ARGUMENTS FOR THE WRAPPER TASK MAY BE PROCESSED HERE!
 parser = argparse.ArgumentParser(description='Command description.')
 
 def main_script(args=None):
     args = parser.parse_args(args=args)
 
     build([
-        Run_{{cookiecutter.graph_folder}}() #execute wrapper task
+        Run_{{cookiecutter.graph_folder}}() #execute wrapper task Run_{{cookiecutter.graph_folder}}
     ], local_scheduler=True)
 
--- {{cookiecutter.graph_folder}}/{{cookiecutter.dag_name}}/tasks/tasks.py
+++ {{cookiecutter.graph_folder}}/{{cookiecutter.dag_name}}/tasks/tasks.py
@@ -2,13 +2,12 @@ import os
 from luigi.contrib.external_program import ExternalProgramTask
 from luigi import format, Parameter, WrapperTask, ExternalTask, Task, LocalTarget
 from luigi.contrib.s3 import S3Target
-from lui_gui.src.target import SuffixPreservingLocalTarget
 from scripts.{{cookiecutter.node3_run_target}} import {{cookiecutter.node3_run_target}}
 
 
 class Run_{{cookiecutter.graph_folder}}(WrapperTask):
     """
-    Wrapper Task - to set Parameters for file download
+    Wrapper Task - to set Parameters for {{cookiecutter.graph_folder}}
     """
     def run(self):
         print("Running Lui_GUI graph {{cookiecutter.graph_folder}}!")
@@ -18,54 +17,49 @@ class Run_{{cookiecutter.graph_folder}}(WrapperTask):
 
 
 class {{cookiecutter.node_2}}(ExternalTask):
-    # Fetch S3Target for External File on S3
+    """
+    {{cookiecutter.node_1}} for External File on S3
+    """
     def output(self):
-        return S3Target("{{cookiecutter.node1_target_entry}}",format=format.Nop) #this is node_1
+        return S3Target(r'{{cookiecutter.node1_target_entry}}', format=format.Nop) # <-- Node {{cookiecutter.node_1}}
 
 
 class {{cookiecutter.node_3}}(Task):
-    # Download to Local Target
+    """
+    Run task {{cookiecutter.node4_run_target}}
+    """
     def requires(self):
-        # Depends on the SavedModel ExternalTask being complete
+        # Depends on the {{cookiecutter.node_2}}() being complete
         return {{cookiecutter.node_2}}()
 
     def run(self):
         {{cookiecutter.node3_run_target}}(self)
-        # Atomically copy the file locally
-        # s3_atomic_download(self)
 
     def output(self):
-        return LocalTarget("{{cookiecutter.node3_output_folder}}", format=format.Nop)
+        return LocalTarget(r"{{cookiecutter.node3_output_folder}}\{{cookiecutter.node1_target_filename}}", format=format.Nop)
 
 
 class {{cookiecutter.node_4}}(ExternalProgramTask):
     """
     Run the external program task {{cookiecutter.node4_run_target}}
     """
-
-    output_folder = '{{cookiecutter.node5_target_output}}'
+    source_image = r'{{cookiecutter.node4_input_params}}' #raw string to avoid escape characters
+    output_folder = r'{{cookiecutter.node5_target_output}}'
+    watermark_location = "topleft"
 
     def requires(self):
         """ Requires {{cookiecutter.node_2}} already downloaded
-        :return model: Local Target of Model
-        Note: passes Luigi Parameters for model
         """
         return {{cookiecutter.node_3}}()
 
     def program_args(self):
-        """ Command line arguments to call external program {{cookiecutter.node_4}}
+        """ Command line arguments to call external program Add_Watermark_Top_Left
         :return args: CLI args
         """
-        return ['{{cookiecutter.node4_run_target}} {{cookiecutter.node4_input_params}} {{cookiecutter.node3_output_folder}} {{cookiecutter.node5_target_output}} topleft']
-
-    def run(self):
-        # create missing directories
-        if not os.path.exists(self.output_dir):  # if missing
-            os.makedirs(self.output_dir) # create dir
-        with self.output().temporary_path() as self.temp_output_path: # atomic write
-            super().run()
+        # pattern: python <target_script.py> <source path> <input path> <output path> <args>
+        return ['python','{{cookiecutter.node4_run_target}}', self.source_image, self.input().path, self.output().path, self.watermark_location]
 
     def output(self):
-        self.file_name = os.path.basename(self.output_folder)
+        self.file_name = os.path.basename(self.source_image)
         self.output_file_name = (os.path.splitext(self.file_name)[0]+"_watermarked"+os.path.splitext(self.file_name)[1])
-        return SuffixPreservingLocalTarget(os.path.join(self.output_folder, self.output_file_name),format=format.Nop) # <-- {{cookiecutter.node_5}}
+        return LocalTarget(os.path.join(self.output_folder, self.output_file_name), format=format.Nop) # <-- {{cookiecutter.node_5}}
